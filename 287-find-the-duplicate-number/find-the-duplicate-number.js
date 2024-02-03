c91750fd1937b2876535216cd83b65f4b0631051
Cody Price <ctprice11@gmail.com>
2024-02-03 16:53:38 -0700

    Time: 190 ms (13.07%) | Memory: 63.5 MB (17.91%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let checkArr = nums.slice();
+    checkArr.sort((a, b) => {
+        if (a < b) {
+            return -1;
+        } else if (a > b) {
+            return 1;
+        }
+        return 0;
+    })
+
+    for (i=0;i<checkArr.length;i++) {
+        if (checkArr[i] === checkArr[i + 1]) {
+            return checkArr[i];
+        } 
+    }
+};
